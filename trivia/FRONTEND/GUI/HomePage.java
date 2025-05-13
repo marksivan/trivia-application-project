bad6e5f9d361f52295e14bd91071e846bcc74665
NiyiIrenee <niyibahoirenee@gmail.com>
2025-05-13 17:50:51 -0400

    Added hover effects

--- trivia/FRONTEND/GUI/HomePage.java
+++ trivia/FRONTEND/GUI/HomePage.java
@@ -176,31 +176,46 @@ public class HomePage extends JPanel {
     }
 
     private JPanel createCategoryCard(String category) {
+        Color defaultColor = GenericComponents.ThemeManager.getButton();
+        Color hoverColor = new Color(0xFFD700); // gold
+    
         JPanel cardPanel = new JPanel(new BorderLayout(20, 20)) {
             @Override
             protected void paintComponent(Graphics g) {
-                Graphics2D g2 = (Graphics2D) g;
+                Graphics2D g2 = (Graphics2D) g.create();
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                 g2.setColor(getBackground());
-                g2.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
-                super.paintComponent(g);
+                g2.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
+                g2.dispose();
             }
         };
-        cardPanel.setBackground(GenericComponents.ThemeManager.getButton());
+        cardPanel.setOpaque(false);
+        cardPanel.setBackground(defaultColor);
         cardPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
         cardPanel.setPreferredSize(new Dimension(300, 200));
+    
         JPanel textPanel = createTextPanel(category);
-
         cardPanel.add(textPanel, BorderLayout.CENTER);
-
+    
         cardPanel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                cardPanel.setBackground(hoverColor);
+                cardPanel.repaint();
+            }
+    
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                cardPanel.setBackground(defaultColor);
+                cardPanel.repaint();
+            }
+    
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 startGame(false, category);
             }
         });
-
+    
         return cardPanel;
     }
+    
 
     private JPanel createTextPanel(String category) {
         JPanel textPanel = new JPanel(new GridBagLayout());
